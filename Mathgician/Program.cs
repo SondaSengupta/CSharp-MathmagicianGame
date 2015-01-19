bae7b2e23c0ea00b788bda54c9da619b3ce01ed5
SondaSengupta <sondasengupta@gmail.com>
2015-01-18 23:40:37 -0600

    Integers and Fibonacci Done

---
+++ Mathgician/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathgician
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I AM THE MATHGICIAN");
+            Console.WriteLine("What shall I math?\nFibonacci, Primes, or Integers");
+            string desiredMaths = Console.ReadLine();
+            Console.WriteLine("Ok.  I'm gonna do some " + desiredMaths);
+            if ("Integers" == desiredMaths)
+            {
+                PrintIntegers();
+            }
+            else if ("Fibonacci" == desiredMaths)
+            {
+                PrintFibonacciSequence();
+            }
+            else if ("Primes" == desiredMaths)
+            {
+                PrintPrimesSequence();
+            }
+            Console.WriteLine("Press Any Key To Exit...");
+            Console.ReadKey();
+        }
+
+        private static void PrintPrimesSequence()
+        {
+
+            Console.WriteLine("Well, Actually... I Lied..");
+        }
+
+        private static void PrintFibonacciSequence()
+        {
+            // 1, 1, 3, 5, 8, 13
+            int previous = 0;
+            int current = 1;
+            while (true)
+            {
+                Console.WriteLine(current);
+                System.Threading.Thread.Sleep(100);
+                int newValue = previous + current;
+                previous = current;
+                current = newValue;
+            }
+        }
+
+        private static void PrintIntegers()
+        {
+            int nextInteger = 0;
+            while (true)
+            {
+                nextInteger++;
+                Console.WriteLine(nextInteger);
+            }
+        }
+    }
+}
